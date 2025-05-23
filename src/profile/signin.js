47f1f70b1c772a29a13ee94f405488150ea4744e
alameesnr <145246885+alameesnr@users.noreply.github.com>
2025-05-24 00:43:10 +0100

    frontend completed

--- src/profile/signin.js
+++ src/profile/signin.js
@@ -1,88 +1,88 @@
-// Signup.jsx
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import '../profile/signin.css';
-import logo from '../asset/Mn11.png';
-import balanceImage from '../asset/lagos.jpg';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+// // Signup.jsx
+// import React, { useState } from 'react';
+// import axios from 'axios';
+// import { useNavigate } from 'react-router-dom';
+// import '../profile/signin.css';
+// import logo from '../asset/Mn11.png';
+// import balanceImage from '../asset/lagos.jpg';
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 
-function Signup() {
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
+// function Signup() {
+//   const [formData, setFormData] = useState({
+//     email: '',
+//     password: '',
     
-  });
-  const navigate = useNavigate();
+//   });
+//   const navigate = useNavigate();
 
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value
-    });
-  };
+//   const handleChange = (e) => {
+//     setFormData({
+//       ...formData,
+//       [e.target.name]: e.target.value
+//     });
+//   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
 
-    try {
-      const response = await axios.post('http://localhost:5000/api/auth/login-staff', formData);
-      localStorage.setItem('staffEmail', response.data.email);
-      toast.success('Signup successful!');
-      console.log(response.data);
-      navigate('/staffing')
-    } catch (err) {
-      toast.error(err.response?.data?.message || 'Signup failed. Please try again.');
-    }
-  };
+//     try {
+//       const response = await axios.post('http://localhost:5000/api/auth/login-staff', formData);
+//       localStorage.setItem('staffEmail', response.data.email);
+//       toast.success('Signup successful!');
+//       console.log(response.data);
+//       navigate('/staffing')
+//     } catch (err) {
+//       toast.error(err.response?.data?.message || 'Signup failed. Please try again.');
+//     }
+//   };
 
-  return (
-    <div className="signup-container">
-      <div className="left-section">
-        <div className="overlay">
-          <img src={logo} alt="Mantra Attorneys LP" className="logo" />
-          <h1 className="brand-text">MANTRA<br />ATTORNEYS LP</h1>
-        </div>
-        <img src={balanceImage} alt="Balance Scales" className="background-img" />
-      </div>
+//   return (
+//     <div className="signup-container">
+//       <div className="left-section">
+//         <div className="overlay">
+//           <img src={logo} alt="Mantra Attorneys LP" className="logo" />
+//           <h1 className="brand-text">MANTRA<br />ATTORNEYS LP</h1>
+//         </div>
+//         <img src={balanceImage} alt="Balance Scales" className="background-img" />
+//       </div>
 
-      <div className="right-section">
-        <div className="form-box">
-        <div className="top-right-heading">
+//       <div className="right-section">
+//         <div className="form-box">
+//         <div className="top-right-heading">
         
-        <a href="/staff" className="admin-link-btn">Admin Signin</a>
-        </div>
-        <h2>Staff Loggin</h2>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="email"
-              name="email"
-              placeholder="Email address"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              value={formData.password}
-              onChange={handleChange}
-              required
-            />
-            {/* <label htmlFor="role">Roles</label>
-            <select name="role" id="role" value={formData.role} onChange={handleChange}>
-              <option value="staff">Staff</option>
-              <option value="admin">Admin</option>
-            </select> */}
-            <button type="submit" className="signup-btn">Sign In</button>
-          </form>
-        </div>
-      </div>
-      <ToastContainer />
-    </div>
-  );
-}
+//         <a href="/staff" className="admin-link-btn">Admin Signin</a>
+//         </div>
+//         <h2>Staff Loggin</h2>
+//           <form onSubmit={handleSubmit}>
+//             <input
+//               type="email"
+//               name="email"
+//               placeholder="Email address"
+//               value={formData.email}
+//               onChange={handleChange}
+//               required
+//             />
+//             <input
+//               type="password"
+//               name="password"
+//               placeholder="Password"
+//               value={formData.password}
+//               onChange={handleChange}
+//               required
+//             />
+//             {/* <label htmlFor="role">Roles</label>
+//             <select name="role" id="role" value={formData.role} onChange={handleChange}>
+//               <option value="staff">Staff</option>
+//               <option value="admin">Admin</option>
+//             </select> */}
+//             <button type="submit" className="signup-btn">Sign In</button>
+//           </form>
+//         </div>
+//       </div>
+//       <ToastContainer />
+//     </div>
+//   );
+// }
 
-export default Signup;
+// export default Signup;
